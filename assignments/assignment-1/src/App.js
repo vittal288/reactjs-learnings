ba416a7a23ac3314213a5dbe6294afcd450d5640
Vittal Kamkar <vittal.kamkar2@mindtree.com>
2018-03-04 11:23:14 +0530

    assignment 1 is completed

--- assignments/assignment-1/src/App.js
+++ assignments/assignment-1/src/App.js
@@ -11,36 +11,48 @@ class App extends Component {
       { username: "Vittal Kamkar" }
     ]
   };
-  
+
   // this should be ES 6 arrow function 
   updateUserNameHandler = () => {
-    console.log('asdada');
     this.setState({
-      names:[
+      names: [
         { username: "New Vittal Kamkar" }
       ]
     })
   }
-  
+
   // event object will pass by react
-  nameChangeHandler = (event)=>{
+  nameChangeHandler = (event) => {
+    // here this keyword will encapsulates and still refer to class "App" but if you plan to handle event listern then you have to ES6 arrowed funtion
     this.setState({
-      names:[
-        {username:event.target.value}
+      names: [
+        { username: event.target.value }
       ]
-    })
+    });
   }
+
+  // in this type of method "this" object behavies differently , if you are not planned to use this then you can use normal method syntax like render(){ }
+  // to handle event listener in this normal method syntax is not possible, throws an error !!
+  // nameChangeHandler(event){
+  //   this.setState({
+  //     names: [
+  //       { username: event.target.value }
+  //     ]
+  //   });
+  // }
   render() {
-    const inputStyle ={
-      border:'2px solid #ff0000'
+    const inputStyle = {
+      border: '2px solid #ff0000'
     }
     return (
       <div className="App">
         <button onClick={this.updateUserNameHandler} style={inputStyle}>Update</button>
-        <UserInput name={this.state.names[0].username} change={this.nameChangeHandler} />       
-        <UserInput  name="Akhila"  change={this.nameChangeHandler} />
+        <UserInput name={this.state.names[0].username} change={this.nameChangeHandler} />
+        
 
         <UserOutPut username={this.state.names[0].username} />
+        <UserOutPut username={this.state.names[0].username} />
+        <UserOutPut username="Akhila" />
       </div>
     );
   }
--- assignments/assignment-1/src/UserOutput/UserOutput.js
+++ assignments/assignment-1/src/UserOutput/UserOutput.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const UserOutPut = (props) =>{
-
+    const pCls ={       
+        fontWeight:'bold',
+        fontSize:'32px',
+        margin:'20px',
+        padding:'20px',
+        border:'2px solid #000',
+        backgroundColor:'#d9d9d9'
+    }
     return(
         <div>
-            <p>{props.username}</p>           
+            <p style={pCls}>{props.username}</p>           
         </div>
     )
 }
