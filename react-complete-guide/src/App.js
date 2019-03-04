5107f2590c35d68a2a7b78a1cf899ac020b0e33d
Vittal KAMKAR <vittal.kamkar@amadeus.com>
2019-03-04 20:50:04 +0530

    SECTION 4 Working with lists and Conditionals is completed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,8 +15,6 @@ class App extends Component {
     showPersons: false,
     'someOtherState': 'Some Other state'
   };
-
-
   // react will pass the event object to this method 
   nameChangedHandler = (event,id) => {
     const personIndex = this.state.persons.findIndex((person)=>{
@@ -26,7 +24,7 @@ class App extends Component {
       return person.id === id;
     });
 
-    // findout exact object needs to be update and create a copy of it 
+    // find out exact object needs to be update and create a copy of it to avoid mutating original state using spread operator 
     const person = {...this.state.persons[personIndex]};
     // OR
     // const person = Object.assign({}, this.state.persons[personIndex]);
@@ -39,6 +37,10 @@ class App extends Component {
   };
 
   tooglePersonHandler = () => {
+    //DO NOT use this way to update/mutate the state of react component 
+    //this.state.persons[1].name ='Vittal Kamkar', instead use setState method
+    
+    
     this.setState({
       showPersons: !this.state.showPersons
     })
@@ -46,8 +48,10 @@ class App extends Component {
 
   deletePersonHandler = (personIndex) =>{
     // this following method as flaw, because ARRAY and OBJECT are reference type or pointer 
-    // --> so in below line of code we mutating the original array of persons by updating the same, hence we are updating the state of it 
+    // --> so in below line of code we mutating the original array of persons by updating the same, hence we are updating the state of react component, supposedly we should not  
     // --> to avoid mutating, we have to create copy of persons, as shown in 2nd approach('New Method')
+
+
     // const persons = this.state.persons;
     // persons.splice(personIndex, 1);
     // console.log('Orginal Person array' , this.state.persons);
@@ -58,8 +62,8 @@ class App extends Component {
 
 
     // @@@ New method 
-    // const persons  = this.state.persons.slice();// ES 5, to copy the array 
-    const persons = [...this.state.persons]; // ES 6 , spread operator will iterate through each array object and bind it into new array 
+    // const persons  = this.state.persons.slice();// ES 5, to copy or clone the array 
+    const persons = [...this.state.persons]; // ES 6 , spread operator will spread through each array item and bind it into new array []
     persons.splice(personIndex,1);
     // console.log('Orginal Person array' , this.state.persons);
     this.setState({persons:persons});
@@ -67,9 +71,6 @@ class App extends Component {
 
   // whenever state updates, react will re render this method 
   render() {
-   
-
-
     let persons = null;
     let btnClass = null;
     if (this.state.showPersons) {
@@ -80,7 +81,7 @@ class App extends Component {
                       click={ () => this.deletePersonHandler(index)}
                       name={person.name} 
                       age={person.age}
-                      key={person.id}
+                      key={person.id}//key is default keyword accept by react component to identify unique of it for virtual DOM
                       changeCustomName={(event)=>this.nameChangedHandler(event, person.id)}/>
           })}        
         </div>
