ab4f0d7910ade932e1d2276ba8a479f908c53761
Vittal KAMKAR <vittal.kamkar@amadeus.com>
2019-03-30 22:22:42 +0530

    LAZY LOADING COMPONENT

--- Routing/routing-react-suspense-start/src/App.js
+++ Routing/routing-react-suspense-start/src/App.js
@@ -9,25 +9,42 @@ const Posts = React.lazy(()=> import('./containers/Posts'));
 
 
 class App extends Component {
+  state={
+    showPosts: false
+  }
+
+  toggleComponent =()=>{
+    this.setState((prevState)=>{
+      return {showPosts: !prevState.showPosts}
+    });
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route 
-            path="/posts" 
-            render={()=>(
-              <Suspense fallback={<div>Loading...</div>}>
-                <Posts/>
-              </Suspense>
-            )} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+      <button onClick={this.toggleComponent}>Toggle Component</button>
+        {this.state.showPosts ?<Suspense fallback={<div>Loading...</div>}>
+                  <Posts/>
+                </Suspense> : <User />
+        }
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route 
+      //       path="/posts" 
+      //       render={()=>(
+      //         <Suspense fallback={<div>Loading...</div>}>
+      //           <Posts/>
+      //         </Suspense>
+      //       )} />
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
